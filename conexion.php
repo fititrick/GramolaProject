d67fb9a4698ad01e427d13af6802a5e96d8e9e1b
IceMupppet <ferpp015@gmail.com>
2013-07-03 16:42:15 +0200

    mierda fallos

--- conexion.php
+++ conexion.php
@@ -1,20 +1,6 @@
-﻿
 <?php 
-/*
-***********************************************************************************************************************************
-Funcion:Realiza la conexion a la base de datos en MySql
-Nombre:conexion.php
-***********************************************************************************************************************************
-*/
 				header("Content-Type: text/html;charset=utf-8");
 				//gramola.sytes.net
-				/*
-					Realizamos la conexion a la base de datos.
-					1º Lugar donde se encuentra.
-					2º Usuario de la base de datos.
-					3º Contraseña.
-
-				*/
 				$conexion=mysqli_connect("gramola.sytes.net", "Gramola", "GramolaPro");
 
 				// Check connection
