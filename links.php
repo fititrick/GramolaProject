0957aaabd3e126c6c7535db42e5a2fe8d4a90df2
IceMupppet <ferpp015@gmail.com>
2013-07-16 15:37:13 +0200

    Cambios visuales fitierrez

--- links.php
+++ links.php
@@ -25,7 +25,7 @@ if($_SESSION["autentificado"]=="SI"){
 	    	 //$line = '<div id="list'.$var.'">'.$row[0].', list nº: '.$row[0].'</div>';	
 	    	 echo '<tr ><td><a id="Link'.$row[2].'" class="link" name='.$row[5].' title='.$row[6].' href="#" >'.$row[0].'</a></td>';
 			 echo '<td >'.$row[3].'</td>';
-			 echo '<td WIDTH=100>'.$row[4].'</td>';
+			 echo '<td >'.$row[4].'</td>';
 			 echo '<td class="linkIcon">'.$row[6].'</td>';
 			 echo '<td class="PosLinkInList">'.$row[1].'</td>';
 			 echo '<td><div class:"votePanel">
